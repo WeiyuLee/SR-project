6b3ce55924d3844314e8676c0aa2144c574ea164
dashmoment <dashmoment1017@gmail.com>
2017-10-13 06:45:47 +0800

    [Project] SR-project
    [What] 	  first create evaluation module
    [Why] 	  For evaluate result on set5, set14, BSD100 and other potential bechmark dataset
    [How] 	  add evaluation.py
    [Risk] none

---
+++ utility/evaluation.py
@@ -0,0 +1,38 @@
+import utility as ut
+
+
+class becnchmark:
+
+	def __init__(self, input, label):
+
+		self.input = input
+		self.label = label
+
+	def psnr(self):
+		return
+
+	def ssim(self):
+		return
+
+	def ifc(self):
+		return
+
+
+img_path = ''
+tmp_img = ut.imread(img_path)
+
+def split_img():
+	return
+
+def evaluation():
+
+	#Build Model 
+	#Retore model
+	#input test img
+	#split img
+	#Model out
+	#merge img
+	#Benchmark
+
+def merge_img():
+	return
